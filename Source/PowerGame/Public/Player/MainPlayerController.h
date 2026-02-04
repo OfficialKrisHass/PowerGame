52cbd4d6190b0dcba4d7a907878f92811f093a75
Chris Hass <81988470+OfficialKrisHass@users.noreply.github.com>
2026-02-04 13:33:44 +0100

    Project Init

---
+++ Source/PowerGame/Public/Player/MainPlayerController.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <CoreMinimal.h>
+#include "GameFramework/PlayerController.h"
+#include "MainPlayerController.generated.h"
+
+UCLASS()
+class POWERGAME_API AMainPlayerController : public APlayerController {
+
+	GENERATED_BODY()
+	
+
+};
